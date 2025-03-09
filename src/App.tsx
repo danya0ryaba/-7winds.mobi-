9d221edcee57bf6f51f752e5f3decb825b599a26
Daniil Ryabenkov <danilryaba0@gmail.com>
2025-03-09 12:57:10 +0300

    fix: correct basename for deploy

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,7 @@ function App() {
     <>
       <Routes>
         <Route path='/' element={<MainLayout />}>
-          <Route path='' element={<Home />} />
+          <Route index element={<Home />} />
           <Route path='*' element={<Error />} />
         </Route>
       </Routes>
--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,4 @@
-
 import { createRoot } from 'react-dom/client'
-
 import App from './App.tsx'
 import { setupStore } from './store/store.ts'
 import { Provider } from 'react-redux'
